dc3ca9d788b93781467e46f53175906668523e6a
박정훈 <junghun5947@gmail.com>
2024-09-14 22:40:09 +0900

    feat: 인증처리 허용

---
+++ src/main/java/com/daou/reviewer/common/auth/AuthConfiguration.java
@@ -0,0 +1,18 @@
+package com.daou.reviewer.common.auth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class AuthConfiguration {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                );
+
+        return http.build();
+    }
+}

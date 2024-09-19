ce05a18e49563923c33fb471c675261ced1b9700
myeongju-kim <kingmj@daou.co.kr>
2024-09-19 17:56:36 +0900

    feat(table): pk wrapper class로 변경, builder 선언

--- src/main/java/com/daou/reviewer/domain/entity/BranchHistory.java
+++ src/main/java/com/daou/reviewer/domain/entity/BranchHistory.java
@@ -1,6 +1,7 @@
 package com.daou.reviewer.domain.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
 
 import java.time.LocalDateTime;
 
@@ -11,11 +12,12 @@ import java.time.LocalDateTime;
  * @fileName : BranchHistory
  * @date : 2024. 9. 19.
  */
+@Getter
 @Entity
 public class BranchHistory {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
 
     @ManyToOne
     @JoinColumn(name = "member_id")
--- src/main/java/com/daou/reviewer/domain/entity/Member.java
+++ src/main/java/com/daou/reviewer/domain/entity/Member.java
@@ -1,6 +1,7 @@
 package com.daou.reviewer.domain.entity;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 /**
  * @author : kingmj
@@ -9,11 +10,13 @@ import jakarta.persistence.*;
  * @fileName : Member
  * @date : 2024. 9. 19.
  */
+@Getter
 @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
 
     @Column(nullable = false)
     private String name;
@@ -24,4 +27,11 @@ public class Member {
     @Column(nullable = false)
     private String password;
     //TODO: 필요 컬럼 정의
+
+    @Builder
+    public Member(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
 }

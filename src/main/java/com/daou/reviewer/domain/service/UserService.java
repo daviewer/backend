377077cb5f90005f2e27531d5abf2229191995b2
myeongju-kim <kingmj@daou.co.kr>
2024-09-19 09:26:26 +0900

    feat(user): user service 생성

---
+++ src/main/java/com/daou/reviewer/domain/service/UserService.java
@@ -0,0 +1,14 @@
+package com.daou.reviewer.domain.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author : kingmj
+ * @description :
+ * @packageName : com.daou.reviewer.domain.service
+ * @fileName : UserService
+ * @date : 2024. 9. 19.
+ */
+@Service
+public class UserService {
+}

503f1b37d00d4a97c48273bc41d0c5bbef9354d1
myeongju-kim <kingmj@daou.co.kr>
2024-09-27 14:50:59 +0900

    feat(security): electron, vue 임시 cors 처리

--- src/main/java/com/daou/reviewer/common/auth/AuthConfiguration.java
+++ src/main/java/com/daou/reviewer/common/auth/AuthConfiguration.java
@@ -5,12 +5,30 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
 public class AuthConfiguration {
+    //TODO: CORS 임시 처리
+    CorsConfigurationSource corsConfigurationSource() {
+        return request -> {
+            CorsConfiguration config = new CorsConfiguration();
+            config.setAllowedHeaders(Collections.singletonList("*"));
+            config.setAllowedMethods(Collections.singletonList("*"));
+            config.setAllowedOriginPatterns(List.of("http://localhost:8081", "http://localhost:8082"));
+            config.setAllowCredentials(true);
+            return config;
+        };
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(corsConfigurer -> corsConfigurer.configurationSource(corsConfigurationSource()))
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().permitAll()

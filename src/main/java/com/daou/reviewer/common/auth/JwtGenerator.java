d734ac2ddb778b23bd6107a9b16aff88b09c2634
myeongju-kim <mjoo1106@naver.com>
2024-10-07 23:55:59 +0900

    feat(auth): token 테스트용 Member Object 생성

---
+++ src/main/java/com/daou/reviewer/common/auth/JwtGenerator.java
@@ -0,0 +1,68 @@
+package com.daou.reviewer.common.auth;
+
+import com.daou.reviewer.domain.entity.Member;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class JwtGenerator {
+    //TODO: env
+    @Value("${jwt.access.secret-key}")
+    private String accessSecretKey;
+    @Value("${jwt.access.expired}")
+    private long accessExpiration;
+    @Value("${jwt.refresh.secret-key}")
+    private String refreshSecretKey;
+    @Value("${jwt.refresh.expired}")
+    private long refreshExpiration;
+
+    public String generateAccessToken(Member member) {
+        long now = System.currentTimeMillis();
+        byte[] keyBytes = Decoders.BASE64.decode(accessSecretKey);
+
+        return Jwts.builder()
+                   .setHeader(createHeader())
+                   .setClaims(createClaims(member))
+                   .setSubject(member.getLoginId())
+                   .setIssuedAt(new Date(now))
+                   .setExpiration(new Date(now + accessExpiration))
+                   .signWith(Keys.hmacShaKeyFor(keyBytes), SignatureAlgorithm.HS256)
+                   .compact();
+    }
+
+    public String generateRefreshToken(Member member) {
+        long now = System.currentTimeMillis();
+        byte[] keyBytes = Decoders.BASE64.decode(refreshSecretKey);
+
+        return Jwts.builder()
+                   .setHeader(createHeader())
+                   .setSubject(member.getLoginId())
+                   .setIssuedAt(new Date(now))
+                   .setExpiration(new Date(now + refreshExpiration))
+                   .signWith(Keys.hmacShaKeyFor(keyBytes), SignatureAlgorithm.HS256)
+                   .compact();
+    }
+
+    private Map<String, Object> createHeader(){
+        Map<String, Object> header = new HashMap<>();
+        header.put("typ", "JWT");
+        header.put("alg", "HS256");
+
+        return header;
+    }
+
+    private Map<String, Object> createClaims(Member member){
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("id", member.getId());
+
+        return claims;
+    }
+}
---
+++ src/main/java/com/daou/reviewer/domain/entity/Member.java
@@ -0,0 +1,11 @@
+package com.daou.reviewer.domain.entity;
+
+import lombok.Getter;
+
+@Getter
+public class Member {
+    private long id;
+    private String loginId;
+    private String email;
+    private String name;
+}
